feb671bb26dead79b61b2565b9a9da145ab83b16
Cesar Leonardo Clemente López <clemclem1991@gmail.com>
2016-09-12 21:32:24 -0500

    Update granmat.py

--- granmat.py
+++ granmat.py
@@ -12,9 +12,6 @@ import subprocess
 import pickle as pl
 import random
 import math
-import numpy as np
-import math as mt
-import pickle
 
 '''
 granMat BETA.
@@ -73,7 +70,7 @@ def packYZ(ly,lz,r,N):
 
 	#WARNINGS: function wont run if:
 		#number of particles is smaller than maximum number of particles that could be contained in one floor. 
-	D=r*2.15
+	D=r*2
 	Dred=D*.5
 	Daug=D*1.1
 	dx=r*2.1
@@ -122,7 +119,7 @@ def packYZ(ly,lz,r,N):
 
 	return partCont, radii, lx, fixedVelArr
 
-def cylinderYZold(particles,radii,fixVelArr,radCyl,c,nSides,lz):
+def cylinderYZ(particles,radii,fixVelArr,radCyl,c,nSides,lz):
 	'''
 	#inserts cylinder obstacle within granular material and displaces particles within the obstacle 
 	geometry to the top of the container. obstacle extends towards z direction with geometry center 
@@ -147,13 +144,13 @@ def cylinderYZold(particles,radii,fixVelArr,radCyl,c,nSides,lz):
 		#number of particles is smaller than maximum number of particles that could be contained in one floor.
 
 
-	maxrad=numpy.amax(radii,axis=0)*1.4;
+	maxrad=numpy.amax(radii,axis=0);
 
 	#determining radius of circle and spheres
 	radCir=radCyl/(1+math.sin(math.pi/nSides));
 	radSph=radCyl-radCir;
 	rp=radCir;
-    
+
 
 	#Finding particles that may be within the intruders
 	ixp=numpy.less(particles[:,0],c[0]+rp+maxrad);
@@ -215,233 +212,4 @@ def cylinderYZold(particles,radii,fixVelArr,radCyl,c,nSides,lz):
 		particles[particlesIn,0]=particles[particlesIn,0]+L*1.1;
 
 
-	return particles,radii,fixVelArr,nCyl
-
-
-# PURPOSE: to determine weather a point falls inside a sphere. Returns a bool.
-
-
-def IsInSphere(xp, xs, r):
-    if(numpy.linalg.norm(xp-xs) < r):
-        return true
-    else:
-        return false
-
-
-# PURPOSE: To compute the porosity (% volume occupied) of a cubic cell, given a
-# series of spheres using montecarlo method.
-
-
-def PorosityCube(xcube, lcube, xspheres, rspheres, N):
-
-    xp = np.random(Npoints, 3)
-    Nspheres = xspheres.shape[0]
-
-    xlow = xcube(0) - lcube/2
-    xhi = xcube(0) + lcube/2
-
-    ylow = xcube(1) - lcube/2
-    yhi = xcube(1) + lcube/2
-
-    zlow = xcube(2) - lcube/2
-    zhi = xcube(2) + lcube/2
-
-    xp[:, 0] = xp[:, 0]*(xhi-xlow)+xlow
-    xp[:, 1] = xp[:, 1]*(yhi-ylow)+ylow
-    xp[:, 2] = xp[:, 2]*(zhi-zlow)+zlow
-
-    Ninside = 0
-
-    # For each point, determine weather it falls inside a sphere
-    for i in range(0, Npoints):
-        ithp = xp[i, :]
-        for j in range(0, Nspheres):
-            if(IsInSphere(ithp, xspheres[j, :], rspheres[j, :])):
-                Ninside = Ninside + 1
-
-    # Return porosity
-    return Ninside/Npoints
-
-def cylinderYZ(particles,radii,fixVelArr,radCyl,c,nSides,lz):
-	'''
-	#inserts cylinder obstacle within granular material and displaces particles within the obstacle 
-	geometry to the top of the container. obstacle extends towards z direction with geometry center 
-	c (cx,cy) and has radius radCyl.  Cylinders are approximated using a regular polygon of 'nSides' 
-	number of sides with spheres of radius 'radSph' centered on each of the polygon corners. The 
-	regular polygon is concentric with the cylinder. the spheres circumference make 
-	contact with the cylinder surface and the neightbour spheres only in one 'point' of space. 
-
-		#input parameters
-		particles
-		radii
-		fixVelArr
-	 
- 
- 	#WARNINGS: If the particles simulating the obstacle are outside boundaries, sphere won't run the simulation.
-	radCyl=dx*3
-	c=numpy.array([lx/2 ,ly/2]) #center of cylinder
-	nSides=15;
-	lz=lz;'''
-
-	#WARNINGS: function wont run if:
-		#number of particles is smaller than maximum number of particles that could be contained in one floor.
-
-
-	maxrad=numpy.amax(radii,axis=0)*1.3;
-
-	#determining radius of circle and spheres
-	radCir=radCyl/(1+math.sin(math.pi/nSides));
-	radSph=radCyl-radCir;
-	rp=radCir;
-    
-
-	#Finding particles that may be within the intruders
-	ixp=numpy.less(particles[:,0],c[0]+rp+maxrad);
-	ixn=numpy.greater(particles[:,0],c[0]-rp-maxrad);
-	iyp=numpy.less(particles[:,1],c[1]+rp+maxrad);
-	iyn=numpy.greater(particles[:,1],c[1]-rp-maxrad);
-
-	particlesIn=ixp*ixn*iyp*iyn;
-
-
-	#Generating coordinates
-	s=numpy.array(numpy.arange(0,nSides)*(2*math.pi/nSides)); #array with Angle values for each of the 
-	xCord=radCir*numpy.cos(s).reshape(nSides,1);
-	yCord=radCir*numpy.sin(s).reshape(nSides,1);
-	zCord=numpy.zeros(nSides).reshape(nSides,1);
-	circleCord=numpy.concatenate((xCord,yCord,zCord),axis=1);
-
-
-	#generating cylinder
-	nCuts=lz/(radSph*2);
-	nCutsInt=int(math.floor(nCuts));
-	freeSpace=(nCuts-nCutsInt)*radSph*2;
-	cylinderCord=numpy.zeros([0,3]);
-	spaceDiv=freeSpace/(nCuts+1)
-
-	for i in range(0,nCutsInt):
-		tempBlock=circleCord;
-		tempBlock[:,2]=i*(radSph*2+spaceDiv)+spaceDiv+radSph;
-		cylinderCord=numpy.concatenate((cylinderCord,tempBlock),axis=0);
-	print "number or cirlces", nCutsInt
-	print "free space is", freeSpace
-	print "space between each circle is", spaceDiv
-	print "coord", cylinderCord[:,2]
-	#Placing cylinder
-	cylinderCord[:,0]=cylinderCord[:,0]+c[0];
-	cylinderCord[:,1]=cylinderCord[:,1]+c[1];
-	cylinderCord[:,2]=cylinderCord[:,2];
-
-
-
-	#generating new radii and fixedVelArr
-	nCyl=nSides*nCutsInt;
-	newRadii=numpy.zeros(nCyl).reshape(nCyl,1);
-	newFixVelArr=newRadii;
-	newParticlesIn=numpy.zeros(nCyl, dtype=bool);
-
-	
-	newRadii=newRadii+radSph;
-	newFixVelArr=newFixVelArr-1;
-
-	#packing all particles together
-	particles=numpy.concatenate((cylinderCord,particles),axis=0);
-	radii=numpy.concatenate((newRadii,radii),axis=0);
-	fixVelArr=numpy.concatenate((newFixVelArr,fixVelArr),axis=0);
-	particlesIn=numpy.concatenate((newParticlesIn,particlesIn),axis=0);
-
-	#computing distance of displacement if there is any
-	if numpy.size(particles[particlesIn,0],axis=0)> 0: 
-		highest=numpy.amax(particles[:,0],axis=0);
-		lowest=numpy.amin(particles[particlesIn,0],axis=0);
-		L=highest-lowest+maxrad*1.01;
-		particles[particlesIn,0]=particles[particlesIn,0]+L*1.1;
-
-	print "Number of particles per cylinder:", nCyl
-	print "Cylinder coordinates", cylinderCord
-	return particles,radii,fixVelArr,nCyl
-
-
-def brakeWalls(radii,particles,fixVelArr,nWalls,lWalls,relativeRes,wallSepX,lx, ly, lz):
-
-	'''
-	radii = vector containing information about particles radius
-	particles = vector containing particle positions
-	fixVelArr = information about particles kind (static/moving)
-	nWalls = Number of obstacle walls
-	lWalls = length of walls
-	relativeRes = sets wall particles size relative to the smallest moving particle in vector particles. e.g. if relativeRes= 		5, particles from walls will be (approximately) 5 times smaller than the smallest moving particle.
-	wallSepX = separation from walls at X dimension (heigth)
-	lx,ly,lz = container dimensions
-
-	'''
-	
-
-	r=np.amin(radii)
-	# Encontrando diametro de esfera
-	rRes=(r/relativeRes)
-	divisor=lWalls/rRes
-	divisorInt=mt.floor(divisor)
-	rem=divisor-divisorInt
-	if rem>0 :
-		diametroExtra=rem*rRes/divisorInt;
-		rRes=rRes+diametroExtra
-
-	wallSphD=rRes
-
-	ny=round(lWalls/wallSphD)
-	nz=lz/wallSphD
-	nzInt=int(mt.floor(nz))
-	rem=nz-nzInt
-
-
-	if rem > 0 :
-		dSepZ=(rem*wallSphD)/(nzInt+1) #Distancia que hay entre cada una de las esferas en Z
-	else :
-		dSepZ=0
-	
-	nSph=int(nzInt*ny)
-	block=np.zeros((nSph,3))
-	tempBlock=block
-	row=np.zeros((ny,3))
-	for i in range(0,int(ny)):
-		row[i,:]=(0,i*wallSphD,0)
-	
-	for i in range(0,nzInt):
-		row[:,2]=i*(wallSphD)+(i+1)*dSepZ
-
-		block[i*ny:(i+1)*ny,:]=row
-
-	wallSepY = (ly - nWalls*lWalls)/(nWalls+1)
-	block[:,1]=block[:,1]+wallSphD/2
-
-	wallsCoord=np.zeros((nWalls*nSph,3))
-	newFixVelArr=np.zeros((nWalls*nSph,1))
-	newFixVelArr[:,0]=-1;
-	newRadii=np.zeros((nWalls*nSph,1))
-	newRadii[:,0]=wallSphD;
-
-	l1=wallSepX+wallSphD
-	l2=wallSphD/2
-
-	for i in range(0,nWalls) :
-		tempBlock=np.zeros((nSph,3))
-		tempBlock[:,1]=block[:,1]+(i+1)*(wallSepY)+i*(lWalls)
-		if i%2 == 0 :
-			tempBlock[:,0]=l1
-		else :
-			tempBlock[:,0]=l2
-		tempBlock[:,2]=block[:,2]
-		wallsCoord[nSph*i:nSph*(i+1),:]=tempBlock;
-		del tempBlock
-
-	# Translating particles
-	L=wallSepX+2*wallSphD
-	particles[:,0]=particles[:,0]+L
-
-	# Packing all particles together
-	particles=np.concatenate((wallsCoord,particles),axis=0);
-	radii=np.concatenate((newRadii,radii),axis=0);
-	fixVelArr=np.concatenate((newFixVelArr,fixVelArr),axis=0);
-	
-	return particles, radii, fixVelArr
+	return particles,radii,fixVelArr
